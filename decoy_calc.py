56725a91667f1f3cc8328427ee6adbee8141703e
sidharthsreelal <sidharthsreelal@gmail.com>
2025-12-22 21:15:07 +0530

    feat: Implement calculator display screen.

--- decoy_calc.py
+++ decoy_calc.py
@@ -1,10 +1,17 @@
 import tkinter as tk
+
 class Calculator:
     def __init__(self, win):
         self.win = win
         win.title("Calculator")
         win.geometry("300x400")
         win.resizable(False, False)
+        
+        self.displaytext = tk.StringVar()
+        self.screen = tk.Entry(win, textvariable=self.displaytext, font=('Arial', 20), bd=10,
+                               insertwidth=2, width=14, borderwidth=4, justify='right')
+        self.screen.grid(row=0, column=0, columnspan=4, pady=10)
+        self.displaytext.set("0")
 
 if __name__ == "__main__":
     root = tk.Tk()
