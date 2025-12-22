74b9fc52621bd938354ba2f3818e3849dc016fe9
sidharthsreelal <sidharthsreelal@gmail.com>
2025-12-22 20:19:25 +0530

    version1

--- decoy_calc.py
+++ decoy_calc.py
@@ -5,3 +5,8 @@ class Calculator:
         win.title("Calculator")
         win.geometry("300x400")
         win.resizable(False, False)
+
+if __name__ == "__main__":
+    root = tk.Tk()
+    calc = Calculator(root)
+    root.mainloop()
